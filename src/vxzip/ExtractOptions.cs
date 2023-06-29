27587a33811bb48ed18283035b7a6affba678d11
CRACKbomber <1568512+CRACKbomber@users.noreply.github.com>
2023-06-29 18:17:00 -0400

    Move vxzip to its own repo.

--- src/vxzip/ExtractOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using CommandLine;
-
-[Verb("extract", HelpText = "Extract the contents of an existing XZP pack file")]
-internal class ExtractOptions : ZipOperation
-{
-    [Option('f', "filter", Default = "*")]
-    public string SearchPattern { get; set; }
-}
--- src/vxzip/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿using vxzip;
-
-ZipApp.Run(args);
